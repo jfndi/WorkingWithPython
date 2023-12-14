5f1e92dc8dd9e3177150ba4a2967daef008ae0b4
jfndi <143571177+jfndi@users.noreply.github.com>
2023-12-14 11:30:49 +0100

    Add pkgmain5.py to illustrate the use of site.addsitedir().

---
+++ PythonForGeeks/01-Modularization/ImportingModules/pkgmain5.py
@@ -0,0 +1,43 @@
+"""
+pkgmain5.py:
+
+    Created on 14-Dec-23
+    
+    @Author: Jean-François Ndi
+"""
+# pkgmain4.py with addsitedir code.
+import os
+import site
+
+dir_path = os.path.dirname(os.path.abspath(__file__))
+assert isinstance(dir_path, str)
+site.addsitedir(dir_path)
+
+import masifutil
+
+def my_main():
+    """
+    This is a main function which generate two random\
+    numbers and then apply calculator functions on them.
+
+    :return: None.
+    """
+    x = masifutil.random_2d()
+    y = masifutil.random_1d()
+
+    sum = masifutil.add(x, y)
+    diff = masifutil.subtract(x, y)
+    sroot = masifutil.sqrt(x)
+    log10x = masifutil.log(x)
+    log2x = masifutil.log2(x)
+
+    print(f'x = {x}, y = {y}')
+    print(f'sum is {sum}')
+    print(f'diff is {diff}')
+    print(f'square root is {sroot}')
+    print(f'log base of 10 is {log10x}')
+    print(f'log base of 2 is {log2x}')
+
+
+if __name__ == '__main__':
+    my_main()
